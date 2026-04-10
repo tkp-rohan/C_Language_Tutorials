01d9b9c6843a5276de125952410ef791e138eb99
tkp-rohan <your_github_email@example.com>
2026-04-10 11:56:39 +0530

    Module-1 completed

--- C_Basics_to_Advanced/M1_Basics/c1m1p1.c
+++ C_Basics_to_Advanced/M1_Basics/c1m1p1.c
@@ -1,5 +1,56 @@
+// Input Devices: Keyboard, Mouse, Scanner. Microphone
+// Output Devices: Printer, Speaker, Monitor
+
+// CPU
+// Memory: Cache, Main Memory, Secondary Memory
+// Cache: Present on the chip, fastest acces time, high cost, low capacity, volatile memory
+
+// Main Memory: RAM, mediocre access time, moderate cost, moderate to low capacity, vollatile memory
+
+// Secondary Memory: Hard Disk. Permanent Storage, non vollatile memory, low access time, lower cost, huge storage capacity
+
+// OS does memory management, hardware management and data transfer, etc
+
+// Compiler: Converts entire source code into machine code all at once.
+
+// Interpreter: Line by Line converts source code to machine code
+
+// Statically Typed Languages: C, C++, Java. Need to mention the data type of variables and it is checked at compile time. Faster
+
+// Dynamically Typed Languages: Python, JavaScript. Need not mention the data type of variables and it is checked at runtime. Slower
+
+// Keywords: Reserved Words whose meaning is pre-defined to the compiler
+
+// Variables and Data Types
+// Functions
+
+// Pre processor: A preprocessor is a program that processes source code before the actual compilation begins, modifying the text based on directives (typically lines starting with #). It handles tasks like macro expansion, file inclusion (#include), and conditional compilation (#ifdef) to enhance code portability and efficiency.
+
+
+// C Source Code ---> Preprocessor ---> Preprocessed C Code ---> Compiler ---> Object Code ---> Linker ---> Executebale Code <--- Loader <--- Libraries
+
+
+// Single Line Comment
+
+/*  Multi
+    Line
+    Comment
+*/
+
+
+
 #include<stdio.h>
 int main(){
     printf("Hello World!");
     return 0;
 }
+
+// Uses of C Programming Language:
+// 1. Low Level Programming
+// 2. Fastest Speed of Execution
+// 3. Requires less resources hence suitable for embedded programming and system softwares.
+// 4. All OS Kernels and DBMS are coded/developed using it.
+// 5. Mother of all programming languages.
+// 6. CPython an implementation of Python is written in C Language.
+
+// C++ is built on top of C with OOP and other advanced features
